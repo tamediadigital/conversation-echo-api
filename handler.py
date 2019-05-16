406cc250b0f42e655f3f387fb0c868c67c3785de
Fabian Mettler <fabian@mettler.cc>
2019-05-16 18:37:45 +0200

    Remove conversation ID from messages list

--- handler.py
+++ handler.py
@@ -6,33 +6,29 @@ def list_conversations(event, context):
     conversations = Conversation.all()
 
     response = {
-        "statusCode": 200,
-        "body": json.dumps(conversations)
+        'statusCode': 200,
+        'body': json.dumps(conversations)
     }
 
     return response
 
 def list_messages(event, context):
     conversation_id = event.get('pathParameters', {}).get('conversation_id')
-
     messages = Message.all(conversation_id)
 
-    body = {
-        "conversation_id": conversation_id
-    }
-
     response = {
-        "statusCode": 200,
-        "body": json.dumps(messages)
+        'statusCode': 200,
+        'body': json.dumps(messages)
     }
 
     return response
 
 def current_user(event, context):
     current_user = User.current()
+
     response = {
-        "statusCode": 200,
-        "body": json.dumps(current_user)
+        'statusCode': 200,
+        'body': json.dumps(current_user)
     }
 
     return response
